8c7025e3a482b2d970e07a8a7992f53b645ef213
doloreel <jarimado@gmail.com>
2016-06-01 08:44:49 +1000

    added users and products controllers

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,17 @@
+class ProductsController < ApplicationController
+    def new
+    end
+
+    def index
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def show
+    
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
     resources :users, :products
+
+    # get 'products/new'
+    #
+    # get 'products/index'
+    #
+    # get 'products/create'
+    #
+    # get 'products/edit'
 end
