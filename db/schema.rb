0b8b17cc451035af88cb15f4db7fcc148eedea3e
doloreel <jarimado@gmail.com>
2016-05-31 21:43:27 +1000

    added and migrated association column to user-id products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531101240) do
+ActiveRecord::Schema.define(version: 20160531113920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160531101240) do
     t.integer "price"
     t.text    "description"
     t.text    "image"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,42 @@ u1  = User.create(
     :image => 'http://lovelace-media.imgix.net/uploads/259/da8e6e30-0a0e-0132-07c7-0eae5eefacd9.jpg?w=700&fit=max&auto=format&q=70',
     :admin => true
 )
+
+u2  = User.create(
+    :name => 'Jake',
+    :email => 'jake@gmail.com',
+    :password => 'chicken',
+    :password_confirmation => 'chicken',
+    :image => 'http://lovelace-media.imgix.net/uploads/259/da8e6e30-0a0e-0132-07c7-0eae5eefacd9.jpg?w=700&fit=max&auto=format&q=70',
+    :admin => true
+)
+
+# t.text      :name
+# t.integer   :price
+# t.text      :description
+# t.text      :image
+
+Product.destroy_all
+
+p1  = Product.create(
+    :name => 'Light Saber Blue',
+    :description => 'You can kill people with this',
+    :image => 'http://lovelace-media.imgix.net/uploads/259/da8e6e30-0a0e-0132-07c7-0eae5eefacd9.jpg?w=700&fit=max&auto=format&q=70'
+)
+
+p2  = Product.create(
+    :name => 'Light Saber Red',
+    :description => 'You can kill people with this',
+    :image => 'http://lovelace-media.imgix.net/uploads/259/da8e6e30-0a0e-0132-07c7-0eae5eefacd9.jpg?w=700&fit=max&auto=format&q=70'
+)
+
+p3  = Product.create(
+    :name => 'Car',
+    :description => 'It drives around and stuff',
+    :image => 'http://lovelace-media.imgix.net/uploads/259/da8e6e30-0a0e-0132-07c7-0eae5eefacd9.jpg?w=700&fit=max&auto=format&q=70'
+)
+
+
+# ASSOCIATIONS
+
+u1.products << p1
