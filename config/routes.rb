b2a1828f441f37a374d9ae7f67ecfc61d9dcb21f
Jeremy Arimado <jarimado@gmail.com>
2016-06-02 09:04:38 +1000

    added Orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
 
+
+
     root 'products#index'
-    resources :users, :products
+    resources :users, :products, :orders
     get 'products/api/orders' => 'products#api_orders'
 
     # post 'orders' => 'products#api_orders', :as => 'orders_path_url'
+
+    # get 'order/index'
+    #
+    # get 'order/show'
+    #
+    # get 'order/new'
+    #
+    # get 'order/create'
 
     # get 'products/new'
     #
