74c04a0ce44c7abf299b4e6e01e3403c29576c4b
Jeremy Arimado <jarimado@gmail.com>
2016-06-01 20:07:28 +1000

    got some customisation going with the version 1.1 docs

--- public/js/script.js
+++ public/js/script.js
@@ -2,35 +2,40 @@ console.log('loaded: script.js');
 
 var ctx = document.getElementById("myChart").getContext("2d");
 
-var data3 = [
-                {
-            	label: 'Temp',
-            	strokeColor: '#A31515',
-            	data: [{ x: new Date('2011-04-11T11:45:00'), y: 25 },
-            		   { x: new Date('2011-04-11T12:51:00'), y: 28 },
-            		   { x: new Date('2011-04-11T14:10:00'), y: 22 },
-                       { x: new Date('2011-04-11T15:15:00'), y: 18 },
-            		   { x: new Date('2011-04-11T17:00:00'), y: 25 },
-            		   { x: new Date('2011-04-11T21:00:00'), y: 24 },
-            		   { x: new Date('2011-04-12T13:00:00'), y: 24 }
-            	]
+var data = [
+            {
+        	label: 'Temp',
+        	strokeColor: '#A31515',
+        	data: [{ x: new Date('2011-04-11T11:45:00'), y: 25 },
+        		   { x: new Date('2011-04-11T12:51:00'), y: 28 },
+        		   { x: new Date('2011-04-11T14:10:00'), y: 22 },
+                   { x: new Date('2011-04-11T15:15:00'), y: 18 },
+        		   { x: new Date('2011-04-11T17:00:00'), y: 25 },
+        		   { x: new Date('2011-04-11T21:00:00'), y: 24 },
+        		   { x: new Date('2011-04-12T13:00:00'), y: 24 }
+        	      ]
             },
             {
             label: 'Temp',
-            strokeColor: '#A31515',
             data: [{ x: new Date('2011-04-11T11:45:00'), y: 10 },
-                   { x: new Date('2011-04-11T12:51:00'), y: 2 },
+                   { x: new Date('2011-04-11T12:51:00'), y: 10 },
                    { x: new Date('2011-04-11T14:10:00'), y: 11 },
                    { x: new Date('2011-04-11T15:15:00'), y: 11 },
-                   { x: new Date('2011-04-11T17:00:00'), y: 5 },
+                   { x: new Date('2011-04-11T17:00:00'), y: 10 },
                    { x: new Date('2011-04-11T21:00:00'), y: 24 },
-                   { x: new Date('2011-04-12T13:00:00'), y: 90 }
-            ]
-        }
+                   { x: new Date('2011-04-12T13:00:00'), y: 20 }
+               ],
+             fillColor: "rgba(151,187,205,0.2)",
+             strokeColor: "rgba(151,187,205,1)",
+             pointColor: "rgba(151,187,205,1)",
+             pointStrokeColor: "#fff",
+             pointHighlightFill: "#fff",
+             pointHighlightStroke: "rgba(151,187,205,1)"
+            }
 
         ];
 
-var myDateLineChart = new Chart(ctx).Scatter(data3, {
+var options = {
     bezierCurve: true,
     showTooltips: true,
     scaleShowHorizontalLines: true,
@@ -38,4 +43,6 @@ var myDateLineChart = new Chart(ctx).Scatter(data3, {
     scaleType: "date",
     scaleLabel: "<%=value%>°C",
     responsive: true
-});
+}
+
+var myDateLineChart = new Chart(ctx).Scatter(data, options);
