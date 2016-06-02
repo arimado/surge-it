db91fbcb74302804bb5918c7aac2d6dc68defcb3
Jeremy Arimado <jarimado@gmail.com>
2016-06-02 15:42:58 +1000

    showing rev_surge and rev_base on chart

--- app/assets/javascripts/product-details.js
+++ app/assets/javascripts/product-details.js
@@ -6,17 +6,27 @@ $(document).ready(function(){
     var ctx = document.getElementById("productChart").getContext("2d");
 
      var chartData = [
-                 {
-                 	label: 'Temp',
-                 	strokeColor: '#A31515',
-                 	data: []
-                },
-                {
-                    label: 'Revenue',
-                   strokeColor: '#A31515',
-                   data: []
-                }
-             ];
+                        {
+                            label: 'Temp',
+                            strokeColor: '#A31515',
+                            data: []
+                        },
+                        {
+                            label: 'Total Revenue',
+                            strokeColor: '#1581a3',
+                            data: []
+                        },
+                        {
+                            label: 'Surge Revenue',
+                            strokeColor: 'rgb(227, 184, 33)',
+                            data: []
+                        },
+                        {
+                            label: 'Base Revenue',
+                            strokeColor: 'rgb(122, 40, 177)',
+                            data: []
+                        }
+                    ];
 
      var options = {
          bezierCurve: true,
@@ -56,6 +66,14 @@ $(document).ready(function(){
                     .forEach(function(price) {
                          myDateLineChart.datasets[1].addPoint(price.x, price.y);
                      })
+                 mapDatesAndPrices(data, 'revenue_surge')
+                    .forEach(function(price) {
+                         myDateLineChart.datasets[2].addPoint(price.x, price.y);
+                     })
+                     mapDatesAndPrices(data, 'revenue_base')
+                        .forEach(function(price) {
+                             myDateLineChart.datasets[3].addPoint(price.x, price.y);
+                         })
 
                  myDateLineChart.update();
 
