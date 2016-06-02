21b0afb994bc0a0369ad1b92ecb013dfe8e1df81
Jeremy Arimado <jarimado@gmail.com>
2016-06-02 10:09:19 +1000

    writing to order table

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,8 +6,14 @@ class OrdersController < ApplicationController
   end
 
   def create
+      order = Order.create order_params
   end
 
   def show
   end
+
+  private
+      def order_params
+        params.require(:order).permit(:product_id, :price, :price_base, :price_surge, :revenue, :revenue_base, :revenue_surge)
+      end
 end
