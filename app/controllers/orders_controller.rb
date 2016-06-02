674882a5893232eb5f84510d0a168c88726e5ccd
Jeremy Arimado <jarimado@gmail.com>
2016-06-02 14:11:18 +1000

    increasing product price on add order submit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,8 +5,26 @@ class OrdersController < ApplicationController
   def index
   end
 
+  # t.text     "product_id"
+  # t.decimal  "price"
+  # t.decimal  "price_base"
+  # t.decimal  "price_surge"
+  # t.decimal  "revenue"
+  # t.decimal  "revenue_base"
+  # t.decimal  "revenue_surge"
+  # t.text     "my_timestamp"
+  # t.datetime "created_at",    null: false
+  # t.datetime "updated_at",    null: false
+
   def create
-     params[:product_id]
+     product_id = params[:product_id]
+     product = Product.find(product_id)
+     product.update :price => product.price + 10
+
+    # => ADD TO ORDERS DB
+    # => UPDATE current product
+    # =>    increment by 10
+
      binding.pry
   end
 
