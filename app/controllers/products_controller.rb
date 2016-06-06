259d21e520022782cac7cff955a8ca55e8007918
Jeremy Arimado <jarimado@gmail.com>
2016-06-06 21:50:43 +1000

    creating products now include price_base and price_max

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,8 +15,22 @@ class ProductsController < ApplicationController
     end
 
     def create
-        product = Product.create product_params
+        product_params
+
+        # Product.create needs to be explicitly specified because price_base
+        # needs to be equal to the initial price
+
+        product = Product.create(
+            :name => product_params[:name],
+            :description => product_params[:description],
+            :image => product_params[:image],
+            :price => product_params[:price],
+            :price_base => product_params[:price],
+            :price_max => product_params[:price_max]
+        )
+
         redirect_to "/products/#{product.id}/details"
+
     end
 
     def edit
@@ -38,6 +52,6 @@ class ProductsController < ApplicationController
 
     private
         def product_params
-            params.require(:product).permit(:name, :price, :description, :image);
+            params.require(:product).permit(:name, :price, :price_max, :description, :image);
         end
 end
